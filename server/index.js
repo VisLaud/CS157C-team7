ba8a15be1fcdf8b1f656acc5a3fa3cf30452c515
Simanta Thapa <44049186+VisLaud@users.noreply.github.com>
2021-04-02 13:02:10 -0400

    deployed the api to heroku

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 import cors from "cors";
 
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -13,13 +15,17 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://Sam:sam@ict.8u6vp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+app.get("/", (req, res) => {
+  res.send("CIT API");
+});
 
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
     app.listen(PORT, () =>
       console.log(`Server Running on Port: http://localhost:${PORT}`)
